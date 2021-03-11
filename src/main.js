ccdd17e0c6e1a9a7626300eda915a4d83e3f2f80
Patrick Kissling <patrick.kissling@gmx.de>
2021-03-11 23:40:59 +0100

    Debug

--- src/main.js
+++ src/main.js
@@ -23,10 +23,7 @@ const client = new AWSAppSyncClient({
   region: 'eu-west-1',
   auth: {
     type: 'OPENID_CONNECT',
-    jwtToken: () => {
-      console.log('auth')
-      Vue.prototype.$auth.auth0Client.getTokenSilently()
-    }
+    jwtToken: () => Vue.prototype.$auth.auth0Client.getIdTokenClaims()
   }
 },{
   defaultOptions: {
