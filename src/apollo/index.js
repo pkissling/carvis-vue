dcce7542adf49f8fc147b9f5c9dd98d84c69f5d7
Patrick Kissling <patrick.kissling@gmx.de>
2021-03-25 15:35:01 +0000

    Add apollo error link

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,27 +1,49 @@
+/* eslint-disable no-console */
 import Vue from 'vue'
-import AWSAppSyncClient from 'aws-appsync'
+import AWSAppSyncClient, { createAppSyncLink } from 'aws-appsync';
 import VueApollo from 'vue-apollo'
 import { obtainJwtToken } from '../auth/utils'
 import { appsyncUrl, appsyncRegion } from '../../app.config'
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import store from '../store/index'
+
 
 Vue.use(VueApollo)
 
-// apollo
-export const apolloClient = new AWSAppSyncClient({
+const errorLink = onError(({ networkError, graphQLErrors }) => {
+  if (graphQLErrors) {
+    const errorMessages = graphQLErrors.map(({ message, locations, path }) => `Message: ${message}, Location: ${locations}, Path: ${path}`)
+    store.dispatch('notifications/error', 'Es ist ein Fehler aufgetreten: ' + errorMessages)
+  }
+
+  if (networkError) {
+    store.dispatch('notifications/error', 'Es gibt Netzwerkprobleme: ' + networkError)
+  }
+})
+
+const appSyncClientConfig = {
   url: appsyncUrl,
   region: appsyncRegion,
   auth: {
     type: 'OPENID_CONNECT',
     jwtToken: async () => obtainJwtToken()
   }
-},
-  {
+}
+const appSyncLink = createAppSyncLink(appSyncClientConfig);
+const link = ApolloLink.from([errorLink, appSyncLink]);
+
+const appSyncClientOptions = {
+  link,
   defaultOptions: {
     watchQuery: {
       fetchPolicy: 'cache-and-network',
     }
   }
-})
+}
+
+export const apolloClient = new AWSAppSyncClient(appSyncClientConfig, appSyncClientOptions)
+
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient
 })
