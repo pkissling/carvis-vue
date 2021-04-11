78c7adb0fde626d16e087dc709f478160b108b9b
Patrick Kissling <patrick.kissling@gmx.de>
2021-04-11 19:33:04 +0000

    With slider group

--- src/components/ImageUpload.vue
+++ src/components/ImageUpload.vue
@@ -1,58 +1,97 @@
 <template>
-  <v-row>
-    <v-col
-      v-for="(image, index) in images"
-      :key="image.id"
-      class="d-flex child-flex"
-      cols="4"
+  <div>
+    <v-slide-group
+      v-model="selectedImage"
+      class="pa-4"
+      show-arrows
+      center-active
     >
-      <v-sheet
-        elevation="7"
-        outlined
+      <v-slide-item
+        v-for="(image, index) in images"
+        :key="image.id"
+        v-slot="{ active, toggle }"
       >
-        <v-img
-          class="img"
-          :src="image.src"
-          contain
-          @click="previewImage(image)"
-        />
-        <v-icon
-          color="accent"
-          @click="deleteImage(image)"
+        <v-card
+          color="grey lighten-1"
+          class="ma-4"
+          height="200"
+          width="200"
+          @click="toggle"
         >
-          mdi-delete
-        </v-icon>
-        <v-icon
-          color="accent"
-          @click="useImageAsThumbnail(image)"
-        >
-          {{ index === 0 ? 'mdi-star' : 'mdi-star-outline' }}
-        </v-icon>
-        <v-icon
-          v-if="index !== 0"
-          color="accent"
-          @click="imageUp(image)"
-        >
-          mdi-arrow-up
-        </v-icon>
-        <v-icon
-          v-if="index !== images.length - 1"
-          color="accent"
-          @click="imageDown(image)"
-        >
-          mdi-arrow-down
-        </v-icon>
-      </v-sheet>
-    </v-col>
+          <v-menu
+            top
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                height="200"
+                max-width="200"
+                :src="image.src"
+                v-bind="attrs"
+                class="text-right pa-2"
+                v-on="on"
+              >
+                <v-btn
+                  v-if="index === 0"
+                  icon
+                  color="primary"
+                >
+                  <v-icon>mdi-star</v-icon>
+                </v-btn>
+              </v-img>
+            </template>
 
-
+            <v-list>
+              <v-list-item @click.stop="imagePreview=image">
+                <v-list-item-icon>
+                  <v-icon>mdi-fullscreen</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Vollbild</v-list-item-title>
+              </v-list-item>
+              <v-list-item
+                v-if="index !== 0"
+                @click="useImageAsThumbnail(image)"
+              >
+                <v-list-item-icon>
+                  <v-icon>mdi-star</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Als Titelbild</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="deleteImage(image)">
+                <v-list-item-icon>
+                  <v-icon>mdi-delete</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Löschen</v-list-item-title>
+              </v-list-item>
+              <v-list-item
+                v-if="index > 1"
+                @click="imageUp(image)"
+              >
+                <v-list-item-icon>
+                  <v-icon>mdi-arrow-left</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Nach vorne</v-list-item-title>
+              </v-list-item>
+              <v-list-item
+                v-if="index !== images.length - 1"
+                @click="imageDown(image)"
+              >
+                <v-list-item-icon>
+                  <v-icon>mdi-arrow-right</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Nach hinten</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-card>
+      </v-slide-item>
+    </v-slide-group>
 
     <ImagePreviewModal
       v-if="imagePreview"
       :image="imagePreview"
       @cancel="imagePreview = null"
     />
-  </v-row>
+  </div>
 </template>
 
 <script>
@@ -73,7 +112,8 @@ export default {
     return {
       loading: false,
       images: [],
-      imagePreview: null
+      imagePreview: null,
+      selectedImage: null
     }
   },
   async created () {
@@ -109,7 +149,7 @@ export default {
       return Promise.all(imageIds.map(id => this.enrichImageSrc(id)))
     },
     async enrichImageSrc(imageId) {
-      return imageService.fetchImageUrl(imageId, 100)
+      return imageService.fetchImageUrl(imageId, 200)
         .then(url => { return { id: imageId, src: url}})
     },
     deleteImage(deleteImage) {
@@ -125,14 +165,16 @@ export default {
     imageUp(image) {
       const from = this.images.indexOf(image)
       this.moveImage(image, from, from - 1)
+      this.selectedImage--
     },
     imageDown(image) {
       const from = this.images.indexOf(image)
       this.moveImage(image, from, from + 1)
+      this.selectedImage++
     },
     moveImage(image, from, to) {
       this.images.splice(from, 1);
-      this.images.splice(to, 0, image);
+      this.images.splice(to, 0, image)
     }
   }
 }
