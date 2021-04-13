41bf8bca38ce0fb0024c411749c0b64bcfe9fa02
Patrick Kissling <patrick.kissling@gmx.de>
2021-04-13 11:57:51 +0000

    Loading indication, sorting

--- src/components/ImageUploadCard.vue
+++ src/components/ImageUploadCard.vue
@@ -46,7 +46,6 @@ export default {
   data () {
     return {
       images: [],
-      imageUploadQueue: [],
       loading: false,
       clearable: false
     }
@@ -56,10 +55,27 @@ export default {
       return this.images ? 'Fahrzeugbilder hinzufügen...' : 'Weitere Fahrzeugbilder hinzufügen...'
     }
   },
+  created() {
+    if (!this.value) {
+      this.images = []
+      return
+    }
+
+    this.value.map((imageId, index) => this.loadExistingImage(imageId, index))
+  },
   methods: {
     async onImageUpload(images) {
-      images.filter(image => !image.processed)
+      const imageUploadPromises = images
+        .filter(image => !image.processed)
         .map(image => this.processUploadedImage(image))
+
+      if (!imageUploadPromises) {
+        return
+      }
+
+      this.loading = true
+      Promise.all(imageUploadPromises)
+        .finally(() => this.loading = false)
     },
     createPreview(imageId, file) {
       return new Promise((resolve) => {
@@ -70,23 +86,33 @@ export default {
             lazySrc: reader.result,
             processed: true
           })
-        };
+        }
         reader.readAsDataURL(file)
       })
     },
     async processUploadedImage(file) {
       const imageId = file.name + file.lastModified
-      this.createPreview(imageId, file)
+      return this.createPreview(imageId, file)
         .then(previewImage => {
           this.images = [...this.images.filter(image => image !== file), previewImage]
-          this.uploadImage(previewImage.id, file)
+          return previewImage
         })
+        .then(previewImage => this.uploadImage(previewImage.id, file))
+
     },
     async uploadImage(previewImageId, file) {
-      imageService.uploadImage(file)
-        .then(uploadedImageId => imageService.fetchImageUrl(uploadedImageId, 200)
-          .then(imageUrl => this.images = [ ...this.images.filter(i => i.id !== previewImageId), { id: uploadedImageId, src: imageUrl, processed: true } ])
-        )
+      const uploadedImageId = await imageService.uploadImage(file)
+      const imageUrl = await imageService.fetchImageUrl(uploadedImageId, 200)
+      this.images = [ ...this.images.filter(i => i.id !== previewImageId), { id: uploadedImageId, src: imageUrl, processed: true } ]
+    },
+    async loadExistingImage(imageId, index) {
+      this.images.push({ id: imageId, index, processed: true })
+      imageService.fetchImageUrl(imageId)
+        .then(imageUrl => {
+          this.images = [ ...this.images.filter(image => image.id !== imageId), { id: imageId, src: imageUrl, index: index, processed: true }]
+            .sort((a,b) => a.index < b.index)
+        })
+
     }
   }
 }
