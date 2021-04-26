039dc0a5824a67134e4c73c569c3b9c8ba34fecb
Patrick Kissling <patrick.kissling@gmx.de>
2021-04-26 18:08:18 +0200

    Add sentry integration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { auth0Domain, auth0ClientId } from '../app.config';
-import { Auth0Plugin } from './auth';
-import apolloProvider from './apollo';
-import vuetify from './plugins/vuetify';
+import { auth0Domain, auth0ClientId } from '../app.config'
+import { Auth0Plugin } from './auth'
+import apolloProvider from './apollo'
+import vuetify from './plugins/vuetify'
 import store from './store'
+import * as Sentry from '@sentry/vue'
+import { Integrations } from '@sentry/tracing'
 
 // auth
 Vue.use(Auth0Plugin, {
@@ -18,7 +20,16 @@ Vue.use(Auth0Plugin, {
         : window.location.pathname
     );
   }
-});
+})
+
+// sentry
+Sentry.init({
+  Vue,
+  dsn: 'https://d6e0397557fd4cefb61c460d0b0e4797@o582664.ingest.sentry.io/5736641',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+  release: process.env.npm_package_version
+})
 
 Vue.config.productionTip = false
 
