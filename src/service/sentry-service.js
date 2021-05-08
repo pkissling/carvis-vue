983f1560c0d766b760aa3bdd28b8631ca047a5d7
Patrick Kissling <patrick.kissling@gmx.de>
2021-05-08 21:00:05 +0000

    Change severity

--- src/service/sentry-service.js
+++ src/service/sentry-service.js
@@ -1,21 +1,15 @@
 import * as Sentry from '@sentry/vue'
 
 export function captureMessage(payload, extras) {
-  const err = new Error(payload)
-  console.error(err, extras)
-  captureException(err, extras)
-}
-
-export function captureException(exception, extras) {
-  if (!extras) {
-    Sentry.captureException(exception)
-  }
-
   Sentry.withScope(scope => {
+
     Object.keys(extras).forEach(key => {
       const value = extras[key]
       scope.setExtra(key, value)
     })
-    Sentry.captureException(exception)
+
+    scope.setLevel('info')
+
+    Sentry.captureMessage(payload)
   })
 }
