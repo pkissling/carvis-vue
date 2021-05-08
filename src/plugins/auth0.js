1605fc407e7b2e0790edc76bde53489c87a62ca9
Patrick Kissling <patrick.kissling@gmx.de>
2021-05-08 14:44:22 +0000

    Remove debugger statement

--- src/plugins/auth0.js
+++ src/plugins/auth0.js
@@ -2,7 +2,6 @@ import { auth0Domain, auth0ClientId } from '../../app.config'
 import { Auth0Plugin } from '../auth'
 
 export default (vue) => {
-  debugger
   vue.use(Auth0Plugin, {
     domain: auth0Domain,
     clientId: auth0ClientId,
