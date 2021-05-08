5439c20b75bea7c7f5cf1efa42709474b365f32c
Patrick Kissling <patrick.kissling@gmx.de>
2021-05-08 10:38:08 +0000

    Fix image reloading on error

--- src/components/PreviewImage.vue
+++ src/components/PreviewImage.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- TODO on error refretch image -->
     <v-img
       :height="height"
       :width="width"
@@ -44,6 +43,7 @@
 <script>
 import FullscreenImageModal from '../modals/FullscreenImageModal'
 import { reloadImage } from '../service/image-service'
+import { captureMessage } from '../service/sentry-service'
 
 export default {
   components: {
@@ -139,14 +139,34 @@ export default {
         this.fullscreen = true
       }
     },
-    async onError() {
-      if (this.imageId && this.height) {
-        reloadImage(this.imageId, this.height)
-          .then(url => this.reloadedSrc = url)
-          .finally(() => this.error = true)
-      } else {
-        this.error = true
+    async onError(url) {
+      captureMessage(`exception caught while resolving url: ${url}`)
+
+      // prevent endless loop
+      if (this.error) {
+        return
       }
+
+      // if there are no image properties, we can not fetch again
+      if (!this.imageId || !this.height) {
+        this.error = true
+        return
+      }
+
+      // try to fetch new image
+      reloadImage(this.imageId, this.height)
+          .then(url => this.reloadedSrc = url)
+          .then(() => this.testReloadedSrc())
+    },
+    async testReloadedSrc() {
+      fetch(this.src)
+        .then(response => {
+          if (!response.ok) {
+            captureMessage(`reloaded image can not be resolved either. url=[${this.reloadedSrc}], imageId=[${this.imageId}], size=[${this.height}]`)
+            this.reloadedSrc = null
+            this.error = true
+          }
+        })
     }
   }
 }
