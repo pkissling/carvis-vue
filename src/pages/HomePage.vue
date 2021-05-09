47dafe8b1bc3778296f9aa93f1c17f2814fd1e21
Patrick Kissling <patrick.kissling@gmx.de>
2021-05-09 09:54:43 +0000

    Less dummy text

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -13,7 +13,7 @@
       />
 
       <v-card-text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquet pellentesque nisi vitae bibendum. Vestibulum vulputate magna non viverra tristique. Proin non lectus urna. Nullam faucibus justo nisl, ac sodales ante pharetra ut. Ut iaculis dapibus euismod. Morbi massa purus, ornare non dolor ac, faucibus convallis justo. Morbi non convallis nisi. Etiam sit amet dolor vel odio gravida luctus. Phasellus mattis, ante vitae fringilla auctor, magna ipsum facilisis massa, eget cursus nunc ante eget diam. Donec efficitur libero quis turpis euismod, in vehicula leo tempor. Suspendisse in aliquet elit. Suspendisse ac lorem dolor. Nunc vel felis sed velit cursus ultricies. Donec non pretium turpis, et sagittis nulla. Quisque et varius arcu, mattis commodo libero. Maecenas ullamcorper sodales fringilla.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquet pellentesque nisi vitae bibendum. Vestibulum vulputate magna non viverra tristique. Proin non lectus urna. Nullam faucibus justo nisl, ac sodales ante pharetra ut. Ut iaculis dapibus euismod. Morbi massa purus, ornare non dolor ac, faucibus convallis justo. Morbi non convallis nisi. Etiam sit amet dolor vel odio gravida luctus. Phasellus mattis, ante vitae fringilla auctor, magna ipsum facilisis massa, eget cursus nunc ante eget diam. Donec efficitur libero quis turpis euismod, in vehicula leo tempor.
       </v-card-text>
     </v-card>
 
