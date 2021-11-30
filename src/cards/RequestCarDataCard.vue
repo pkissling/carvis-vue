9a9624e42d54069dc2d7c895d579829f9e919631
Patrick Kissling <patrick.kissling@de.bosch.com>
2021-11-30 15:28:54 +0100

    Add all requests fields

--- src/cards/RequestCarDataCard.vue
+++ src/cards/RequestCarDataCard.vue
@@ -24,21 +24,192 @@
           required
         />
       </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.modelSeries"
+          label="Modellreihe"
+          hint="Beispiel: 964, T2, E34"
+        />
+        <TextField
+          v-model="value.modelYear"
+          label="Modelljahr"
+          hint="Beispiel: 1990"
+        />
+      </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.modelDetails"
+          label="Modellspezifikation"
+          hint="Beispiel: RSR Recreation, Carrera RS, Carrera 4"
+        />
+        <TextField
+          v-model="value.bodyType"
+          label="Karosserie"
+          hint="Beispiel: Coupe, Targa, Cabrio"
+        />
+      </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.capacity"
+          type="number"
+          label="Hubraum"
+          hint="Beispiel: 2500"
+          suffix="ccm"
+        />
+        <TextField
+          v-model="value.transmission"
+          label="Getriebe"
+          hint="Beispiel: Automatik, 4Gang, 5Gang"
+        />
+      </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.horsePower"
+          type="number"
+          label="Leistung"
+          suffix="PS"
+        />
+        <TextField
+          :value="kilowatts"
+          type="number"
+          label="Leistung"
+          suffix="KW"
+          @input="kilowattsChanged"
+        />
+      </v-row>
+
+      <v-row>
+        <Dropdown
+          v-model="value.colorExterior"
+          :items="options.colors"
+          label="Außenfarbe"
+        />
+        <TextField
+          v-model="value.colorExteriorManufacturer"
+          label="Hersteller Farbbezeichnung"
+        />
+      </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.colorAndMaterialInterior"
+          label="Innenfarbe"
+        />
+        <Dropdown
+          v-model="value.condition"
+          :items="options.conditions"
+          label="Zustand"
+        />
+      </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.countryOfOrigin"
+          label="Auslieferungsland"
+        />
+        <TextField
+          v-model="value.mileage"
+          label="Laufleistung"
+          type="number"
+          suffix="km"
+        />
+      </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.budget"
+          label="Verfügbares Budget"
+          type="number"
+          suffix="€"
+        />
+        <TextField
+          v-model="value.additionalEquipment"
+          label="Mehr-Ausstattung"
+        />
+      </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.highlights"
+          label="Besonderheiten / Highlights"
+        />
+        <TextField
+          v-model="value.vision"
+          label="Ziel / Vision"
+        />
+      </v-row>
+
+      <v-row>
+        <TextField
+          v-model="value.mustHaves"
+          label="Must-Haves"
+        />
+        <TextField
+          v-model="value.noGos"
+          label="No-Gos"
+        />
+      </v-row>
+
+      <v-row>
+        <TextArea
+          v-model="value.description"
+          label="Detailbeschreibung"
+        />
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-import TextField from '../fields/TextField.vue'
+import TextField from '@/fields/TextField.vue'
+import Dropdown from '@/fields/Dropdown.vue'
+import TextArea from '@/fields/TextArea.vue'
 
 export default {
   components: {
-    TextField
+    TextField,
+    Dropdown,
+    TextArea
   },
   props: {
     value: {
       type: Object,
       default: null
+    }
+  },
+  data: () => {
+    return {
+       options: {
+        colors: [
+          "Beige",
+          "Blau",
+          "Braun",
+          "Gelb",
+          "Gold",
+          "Grau",
+          "Grün",
+          "Orange",
+          "Rot",
+          "Schwarz",
+          "Silber",
+          "Violett",
+          "Weiß",
+          "Andere"
+        ],
+        conditions: [
+          "ursprünglich Original",
+          "Patiniert",
+          "Überwiegend original",
+          "Teilrestauriert",
+          "Restauriert",
+          "Restaurationsobjekt",
+          "Umbau / Backdate"
+        ]
+      }
     }
   },
   computed: {
