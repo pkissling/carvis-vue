521f972a50c178822152d38d19fc16080a5a0bb0
Patrick Kissling <patrick.kissling@de.bosch.com>
2022-04-21 11:19:12 +0200

    Add breadcrumbs

--- src/components/pages/Page.vue
+++ src/components/pages/Page.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <v-row>
+      <v-col>
+        <v-breadcrumbs
+          v-if="renderBreadCrumbs"
+          :items="breadcrumbs"
+        >
+          <template #divider>
+            <v-icon>mdi-chevron-right</v-icon>
+          </template>
+        </v-breadcrumbs>
+      </v-col>
+    </v-row>
     <v-row v-if="title">
       <v-col class="text-h2"
              align="center"
@@ -17,13 +29,30 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    title: {
-      type: String,
-      default: null
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Page extends Vue {
+  @Prop({ required: true })
+  title!: string
+
+  get breadcrumbs(): { text: string, disabled: boolean, href: string }[] {
+    const breadcrumbs = this.$route?.meta?.breadcrumbs?.call(this, this.$route)
+    if (!breadcrumbs?.length) {
+      return []
     }
+    return breadcrumbs.map((item: { text: string, href: string}, i: number) => {
+      return {
+        text: item.text,
+        href: item.href,
+        disabled: breadcrumbs.length -1 === i
+      }
+    })
+  }
+
+  get renderBreadCrumbs(): boolean {
+    return this.breadcrumbs.length > 1
   }
 }
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,68 +5,116 @@ import { authGuard } from '@/auth/auth-guard'
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        component: () => import('@/components/pages/HomePage.vue'),
-        meta: {
-            requiresNoAuth: true,
-        },
+  {
+    path: '/',
+    component: () => import('@/components/pages/HomePage.vue'),
+    meta: {
+      requiresNoAuth: true,
     },
-    {
-        path: '/cars',
-        component: () => import('@/components/pages/CarsPage.vue'),
+  },
+  {
+    path: '/cars',
+    component: () => import('@/components/pages/CarsPage.vue'),
+    meta: {
+      breadcrumbs() {
+        return [
+          { text: 'Fahrzeuge', href: '/cars' },
+        ]
+      }
+    }
+  },
+  {
+    path: '/cars/add',
+    component: () => import('@/components/pages/CreateCarPage.vue'),
+    meta: {
+      breadcrumbs() {
+        return [
+          { text: 'Fahrzeuge', href: '/cars' },
+          { text: 'Fahrzeug hinzufügen', href: '/cars/add' }
+        ]
+      }
+    }
+  },
+  {
+    path: '/cars/:carId',
+    component: () => import('@/components/pages/CarDetailPage.vue'),
+    props: true,
+    meta: {
+      breadcrumbs(route: Route) {
+        const carId = route.params.carId
+        return [
+          { text: 'Fahrzeuge', href: '/cars' },
+          { text: carId, href: `/cars/${carId}` },
+        ]
+      }
+    }
+  },
+  {
+    path: '/forbidden',
+    component: () => import('@/components/pages/ForbiddenPage.vue'),
+    meta: {
+      requiresNoAuth: true,
     },
-    {
-        path: '/cars/add',
-        component: () => import('@/components/pages/CreateCarPage.vue'),
+  },
+  {
+    path: '/requests',
+    component: () => import('@/components/pages/RequestsPage.vue'),
+    meta: {
+      breadcrumbs() {
+        return [
+          { text: 'Gesuche', href: '/requests' },
+        ]
+      }
+    }
+  },
+  {
+    path: '/requests/add',
+    component: () => import('@/components/pages/CreateRequestPage.vue'),
+    meta: {
+      breadcrumbs() {
+        return [
+          { text: 'Gesuche', href: '/requests' },
+          { text: 'Gesuche hinzufügen', href: '/requests/add' },
+        ]
+      }
+    }
+  },
+  {
+    path: '/requests/:requestId',
+    component: () => import('@/components/pages/RequestDetailPage.vue'),
+    props: true,
+    meta: {
+      breadcrumbs(route: Route) {
+        const requestId = route.params.requestId
+        return [
+          { text: 'Gesuche', href: '/requests' },
+          { text: requestId, href: `/requests/${requestId}` },
+        ]
+      }
+    }
+  },
+  {
+    path: '/not-found',
+    name: 'NotFound',
+    component: () => import('@/components/pages/NotFoundPage.vue'),
+  },
+  {
+    path: '*',
+    beforeEnter: (href: Route, from: Route, next: NavigationGuardNext) => {
+      next('/not-found')
     },
-    {
-        path: '/cars/:carId',
-        component: () => import('@/components/pages/CarDetailPage.vue'),
-        props: true,
-    },
-    {
-        path: '/forbidden',
-        component: () => import('@/components/pages/ForbiddenPage.vue'),
-        meta: {
-            requiresNoAuth: true,
-        },
-    },
-    {
-        path: '/requests',
-        component: () => import('@/components/pages/RequestsPage.vue'),
-    },
-    {
-        path: '/requests/add',
-        component: () => import('@/components/pages/CreateRequestPage.vue'),
-    },
-    {
-        path: '/requests/:requestId',
-        component: () => import('@/components/pages/RequestDetailPage.vue'),
-        props: true,
-    },
-    {
-        path: '/not-found',
-        name: 'NotFound',
-        component: () => import('@/components/pages/NotFoundPage.vue'),
-    },
-    {
-        path: '*',
-        beforeEnter: (to: Route, from: Route, next: NavigationGuardNext) => {
-            next('/not-found')
-        },
-    },
+  },
 ]
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes,
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
 })
 
 router.beforeEach((to: Route, from: Route, next: NavigationGuardNext) => {
-    if (to.meta?.requiresNoAuth) next()
-    else authGuard(to, from, next)
+  if (to.meta?.requiresNoAuth) next()
+  else authGuard(to, from, next)
 })
 
 export default router
